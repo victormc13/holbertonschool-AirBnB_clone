3a511a809fa17698c11124d1209c1ca039875d18
huion0905 <valery.armas@upch.pe>
2023-11-27 21:09:58 -0800

    Adds the save and to_dict public instance methods

--- models/base_model.py
+++ models/base_model.py
@@ -38,6 +38,20 @@ class BaseModel:
         """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
+    def save(self):
+        """
+        Updates the 'updated_at' attribute with the current datetime.
+        """
+        self.updated_at = datetime.now()
 
-
-
+    def to_dict(self):
+        """
+        Converts the instance to a dictionary for serialization.
+        Returns:
+            dict: Dictionary representation of the instance.
+        """       
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
