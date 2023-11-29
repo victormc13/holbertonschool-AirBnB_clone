376f916fc3507caf822fd4cff3bf2ac386275c9d
victormc13 <viktorsb14@gmail.com>
2023-11-29 12:59:07 -0800

    Fix pycodestyle coding style identation rules

--- models/base_model.py
+++ models/base_model.py
@@ -58,7 +58,7 @@ class BaseModel:
         Updates the 'updated_at' attribute with the current datetime.
         """
         self.updated_at = datetime.now()
-	storage.save()
+        storage.save()
 
     def to_dict(self):
         """
