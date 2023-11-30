de159687f76752cee3ba8dec2717fcf01cee01fb
huion0905 <valery.armas@upch.pe>
2023-11-30 09:06:31 -0800

    Adds the destroy method that deletes an instance based on the class name and id

--- console.py
+++ console.py
@@ -51,7 +51,23 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     print(storage.all()[args[0]][key])
 
-
+	def do_destroy(self, arg):
+		"""Deletes an instance based on the class name and id."""
+		if not arg:
+            print("** class name missing **")
+        else:
+            args = arg.split()
+            if args[0] not in storage.all():
+                print("** class doesn't exist **")
+            elif len(args) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(args[0], args[1])
+                if key not in storage.all()[args[0]]:
+                    print("** no instance found **")
+                else:
+                    del storage.all()[args[0]][key]
+                    storage.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
