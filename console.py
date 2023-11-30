655d8fbf06cc09c28b37fab27056aa5663c6fe7e
huion0905 <valery.armas@upch.pe>
2023-11-29 20:38:37 -0800

    Make the file executable

---
+++ console.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb)'
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """An empty line + ENTER shouldn't execute anything"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
