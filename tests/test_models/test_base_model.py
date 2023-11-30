89a953ae39b006d071b6b63d93cb8a415fc9b1a5
huion0905 <valery.armas@upch.pe>
2023-11-29 17:44:05 -0800

    Adds new test cases for the save(self) method of the BaseModel class

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,12 +1,25 @@
 import unittest
+import os
 from datetime import datetime
+from models import storage
 from models.base_model import BaseModel
-
 
 class TestBaseModel(unittest.TestCase):
     """
     Test cases for the BaseModel class.
     """
+    def setUp(self):
+    """Set up the test environment."""
+	self.file_path = "test_base_model_save.json"
+        self.storage = storage
+        self.storage._FileStorage__file_path = self.file_path
+        self.model = BaseModel()
+    
+    def tearDown(self):
+        """Clean up after the test."""
+        if os.path.exists(self.file_path):
+            os.remove(self.file_path)
+
     def test_init(self):
         """
         Test the initialization of a BaseModel instance.
@@ -35,6 +48,17 @@ class TestBaseModel(unittest.TestCase):
         my_model.save()
         self.assertNotEqual(my_model.updated_at, original_updated_at)
 
+    def test_save_persists_to_file_storage(self):
+        """Test if save() persists the BaseModel instance to the file storage."""
+        self.model.save()
+        loaded_storage = FileStorage()
+        loaded_storage.reload()
+        all_objs = loaded_storage.all()
+        key = "BaseModel.{}".format(self.model.id)
+        self.assertIn(key, all_objs)
+        loaded_model = all_objs[key]
+        self.assertEqual(loaded_model.updated_at, self.model.updated_at)
+
     def test_to_dict(self):
         """
         Test the to_dict method for correct dictionary representation.
@@ -47,7 +71,6 @@ class TestBaseModel(unittest.TestCase):
         self.assertIn('updated_at', my_model_dict)
         my_model.__dict__['name'] = "Test Model"
         self.assertIn('name', my_model.__dict__)
-
 
 if __name__ == '__main__':
     unittest.main()
