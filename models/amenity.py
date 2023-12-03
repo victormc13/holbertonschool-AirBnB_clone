0344b62e01c4816b7ea37f1f61a7d6de22ff32f0
victormc13 <viktorsb14@gmail.com>
2023-12-02 21:46:14 -0800

    Fix pycodestyle coding style identation rules

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-	""" Amenity class that inherits from BaseModel to represent an amenity """
-	name = ""
+    """ Amenity class that inherits from BaseModel to represent an amenity """
+    name = ""
