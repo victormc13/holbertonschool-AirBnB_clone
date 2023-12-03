d3919313dc1940df13463d3f7778dbf23d4b36ee
huion0905 <valery.armas@upch.pe>
2023-12-03 15:29:24 -0800

    Update the command interpreter to allow those actions: show, create, destroy, update and all with all classes created previously

--- console.py
+++ console.py
@@ -4,6 +4,11 @@
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from models import storage
 
 
