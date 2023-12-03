a9688e36eb2e363456e30477d0abd21e8cd93d2b
huion0905 <valery.armas@upch.pe>
2023-12-03 14:39:55 -0800

    Adds test cases for State class that inherits from BaseModel to represent a state

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import unittest
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+
+    def test_state_instance(self):
+		"""Tests if state is an instance of State class and has the correct attributes with an empty value"""
+        state = State()
+        self.assertIsInstance(state, State)
+        self.assertTrue(hasattr(state, "name"))
+        self.assertEqual(state.name, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
