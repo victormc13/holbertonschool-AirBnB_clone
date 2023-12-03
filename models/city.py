859867efbf4710c75c47a2ac715d5ba44a33aea8
huion0905 <valery.armas@upch.pe>
2023-12-02 21:29:22 -0800

    Creates the City class to represent a city

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+	""" City class that inherits from BaseModel to represent a city. """
+		state_id = ""
+		name = ""
