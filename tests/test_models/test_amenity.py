a6526cd90b9184c066d66a3a35d160875d3576f5
huion0905 <valery.armas@upch.pe>
2023-12-03 14:27:35 -0800

    Adds test cases for Amenity class that inherits from BaseModel to represent an amenity

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import unittest
+from models.amenity import Amenity
+
+class TestAmenity(unittest.TestCase):
+
+    def test_amenity_instance(self):
+		"""Tests if amenity is an instance of Amenity and has the correct attributes. """
+        amenity = Amenity()
+        self.assertIsInstance(amenity, Amenity)
+        self.assertTrue(hasattr(amenity, "name"))
+        self.assertEqual(amenity.name, "")
+
+if __name__ == '__main__':
+    unittest.main()
