e05b568d4d16e56c880892bff503c9ee66d4d0fd
huion0905 <valery.armas@upch.pe>
2023-12-02 21:38:32 -0800

    Creates the Review class to represent a review

---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+	""" Review class that inherits from BaseModel to represent a review """
+	place_id = ""
+	user_id = ""
+	text = ""
