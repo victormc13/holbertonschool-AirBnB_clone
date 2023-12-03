2e12457d17daf43bd5c5a8b9beb4e78086337cbc
huion0905 <valery.armas@upch.pe>
2023-12-02 21:22:01 -0800

    Creates the Place class that represents a place

---
+++ models/place.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+	""" Place class that inherits from BaseModel to represent a place """
+	city_id = ""
+	user_id = ""
+	name = ""
+	description = ""
+	number_rooms = int(0)
+	number_bathrooms = int(0)
+	max_guest = int(0)
+	price_by_night = int(0)
+	latitude = float(0.0)
+	longitude = float(0.0)
+	amenity_ids = []
+
+	
+
