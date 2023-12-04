be18494593b582325a20d5f5f600a52846d36393
huion0905 <valery.armas@upch.pe>
2023-12-03 16:58:40 -0800

    Creates the updated_at attribute for updated time

--- models/base_model.py
+++ models/base_model.py
@@ -41,6 +41,7 @@ class BaseModel:
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
+			self.updated_at = self.created_at
             models.storage.new(self)
 
     def __str__(self):
