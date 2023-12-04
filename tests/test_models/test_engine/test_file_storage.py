ece3f640b2e54c5a28cba8e82c359d11750fd5e1
victormc13 <viktorsb14@gmail.com>
2023-12-03 20:49:07 -0800

    Fix pycodestyle coding style identation rules

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,37 +7,37 @@ import os
 
 class TestFileStorage(unittest.TestCase):
     def setUp(self):
-		"""Set up the test environment before each test case."""
+        """Set up the test environment before each test case."""
         self.file_path = "test_file.json"
         self.storage = FileStorage()
         self.storage._FileStorage__file_path = self.file_path
         self.model = BaseModel()
 
     def tearDown(self):
-		"""Clean up the test environment after each test case."""
+        """Clean up the test environment after each test case."""
         if os.path.exists(self.file_path):
             os.remove(self.file_path)
 
     def test_all(self):
-		"""Test the 'all' method of FileStorage."""
+        """Test the 'all' method of FileStorage."""
         all_objs = self.storage.all()
         self.assertIsInstance(all_objs, dict)
 
     def test_new(self):
-		"""Test the 'new' method of FileStorage."""
+        """Test the 'new' method of FileStorage."""
         self.storage.new(self.model)
         all_objs = self.storage.all()
         key = "BaseModel.{}".format(self.model.id)
         self.assertIn(key, all_objs)
 
     def test_save(self):
-		"""Test the 'save' method of FileStorage."""
+        """Test the 'save' method of FileStorage."""
         self.storage.new(self.model)
         self.storage.save()
         self.assertTrue(os.path.exists(self.file_path))
 
     def test_reload(self):
-		"""Test the 'reload' method of FileStorage."""
+        """Test the 'reload' method of FileStorage."""
         self.storage.new(self.model)
         self.storage.save()
         loaded_storage = FileStorage()
