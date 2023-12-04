5146d3dee140d2fe91f66c4998543ef65d98e4c8
victormc13 <viktorsb14@gmail.com>
2023-12-03 17:04:04 -0800

    Fix pycodestyle coding style identation rule

--- models/base_model.py
+++ models/base_model.py
@@ -41,7 +41,7 @@ class BaseModel:
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
-			self.updated_at = self.created_at
+            self.updated_at = self.created_at
             models.storage.new(self)
 
     def __str__(self):
