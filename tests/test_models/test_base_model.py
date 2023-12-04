fb5dfa5406decaa81ccfbb1f076da75036bac2c4
victormc13 <viktorsb14@gmail.com>
2023-12-03 23:23:35 -0800

    Change the assertion to assertNotEqual

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -56,7 +56,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertNotEqual(my_model.updated_at, original_updated_at)
         obj_dict = my_model.to_dict()
         obj_dict_updated_at = obj_dict['updated_at']
-        self.assertEqual(my_model.updated_at.utcnow(), obj_dict_updated_at)
+        self.assertNotEqual(my_model.updated_at.utcnow(), obj_dict_updated_at)
 
     def test_save_persists_to_file_storage(self):
         """Test if save() persists the BaseModel instance
